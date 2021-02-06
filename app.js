de2336bd811b2c2891b80441143041c8460be9c1
Katrina Nguyen <katrinan@usc.edu>
2021-02-05 21:50:21 -0800

    updating

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-d3.json('../data/samples.json').then(importedData => {
+d3.json('./samples.json').then(importedData => {
 
   console.log(importedData)
   const dropDownMenu = document.getElementById('selDataset');
